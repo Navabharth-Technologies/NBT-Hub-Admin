1df2669639a98293e369d606f49b5c6ec89d156a
sahana-gowda01 <sahana@navabharathtechnologies.com>
2026-05-20 16:23:27 +0530

    fix: resolve page refresh 404, redirect issues, and login updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './AuthContext.js'
 import { ThreadProvider } from './ThreadContext.js'
@@ -9,9 +9,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <ThreadProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ThreadProvider>
     </AuthProvider>
   </React.StrictMode>,
